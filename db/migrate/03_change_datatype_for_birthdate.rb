c8a6c19a7e375de7a69a3c1a0e6d56a1b2a00170
Trever Andersen <trever.r.andersen@gmail.com>
2017-11-17 18:29:36 +0000

    automatically backed up by learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,5 +1,5 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration
   def change
-    add_column :students, :birthdate, :datetime
+    change_column :students, :birthdate, :datetime
   end
 end
